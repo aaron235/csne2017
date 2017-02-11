49363d2a0dde158332a82b348de43f0d85f1c433
Aaron <qwertyman159@gmail.com>
2017-02-11 10:23:34 -0800

    Added the first route

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import render_template
 
 # this is our webapp object
-app = Flask(__name__)
+app = Flask( __name__ )
 
 # set some app settings for convenience
 app.debug = True
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
+
+
+@app.route( '/' )
+def main():
+	return render_template( 'main.html' )
+
 
 app.run()
